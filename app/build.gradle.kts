8054789ac61f06e262f62cf8b1bdf11895238875
NikaNadaraia <nadaraia.nikoloz@kiu.edu.ge>
2023-09-05 18:15:36 +0400

    Added WindowStateUtils.kt, which has enum class according to which Screen will show only player list or both player and card lists (dependant on screen width). also added dependencies for build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -59,6 +59,9 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:${rootProject.extra["lifecycle_version"]}")
+    implementation("androidx.compose.material3:material3-window-size-class")
+
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
---
+++ app/src/main/java/com/example/czechfoolapp/ui/util/WindowStateUtils.kt
@@ -0,0 +1,7 @@
+package com.example.czechfoolapp.ui.util
+
+
+enum class GameContentType {
+    PLAYER_LIST_ONLY,
+    PLAYER_AND_CARD_LISTS
+}
