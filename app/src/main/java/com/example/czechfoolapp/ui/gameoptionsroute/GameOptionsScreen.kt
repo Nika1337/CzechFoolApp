b2bddc2611c50043be20705ec22b5d5298b3871b
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2023-10-07 20:19:38 +0400

    removed unused imports

--- app/src/main/java/com/example/czechfoolapp/ui/gameoptionsroute/GameOptionsScreen.kt
+++ app/src/main/java/com/example/czechfoolapp/ui/gameoptionsroute/GameOptionsScreen.kt
@@ -4,7 +4,6 @@ import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
@@ -38,7 +37,6 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.LocalDensity
 import androidx.compose.ui.platform.LocalFocusManager
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
