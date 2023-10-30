e6f95ecbceeca344ea3621adb908dd611289049c
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2023-10-30 20:04:29 +0400

    implemented DAOs

---
+++ app/src/main/java/com/example/czechfoolapp/database/dao/GameDao.kt
@@ -0,0 +1,25 @@
+package com.example.czechfoolapp.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.czechfoolapp.database.model.GameEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface GameDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insert(game: GameEntity)
+
+    @Delete
+    suspend fun delete(game: GameEntity)
+
+    @Update
+    suspend fun update(game: GameEntity)
+
+    @Query("Select * FROM game")
+    fun getAll(): Flow<List<GameEntity>>
+}
---
+++ app/src/main/java/com/example/czechfoolapp/database/dao/PlayerDao.kt
@@ -0,0 +1,28 @@
+package com.example.czechfoolapp.database.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.czechfoolapp.database.model.PlayerEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface PlayerDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vararg players: PlayerEntity)
+
+    @Delete
+    suspend fun delete(player: PlayerEntity)
+
+    @Update
+    suspend fun update(player: PlayerEntity)
+
+    @Query("SELECT * From player WHERE game_id = :gameID")
+    fun getAllPlayersInGameSpecified(gameID: Int) : Flow<List<PlayerEntity>>
+
+    @Query("SELECT * FROM player WHERE game_id = :gameID AND name = :name")
+    fun getPlayer(gameID: Int, name: String) : PlayerEntity
+}
