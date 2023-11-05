e5674cd7f6e543adc091b8b17f1e3a0a6d474fbf
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2023-11-05 13:14:30 +0400

    added exception handling in OfflinePlayersRepository

--- app/src/main/java/com/example/czechfoolapp/data/repository/OfflinePlayersRepository.kt
+++ app/src/main/java/com/example/czechfoolapp/data/repository/OfflinePlayersRepository.kt
@@ -1,5 +1,7 @@
 package com.example.czechfoolapp.data.repository
 
+import android.database.sqlite.SQLiteAbortException
+import android.database.sqlite.SQLiteConstraintException
 import com.example.czechfoolapp.data.model.Player
 import com.example.czechfoolapp.data.model.toPlayerEntity
 import com.example.czechfoolapp.database.dao.PlayerDao
@@ -10,15 +12,22 @@ import kotlinx.coroutines.flow.map
 class OfflinePlayersRepository(
     private val playerDao: PlayerDao
 ) : PlayersRepository {
-    override suspend fun insertAll(vararg players: Player) =
+    override suspend fun insertAll(vararg players: Player) = try {
         playerDao.insertAll(*(players.map { it.toPlayerEntity() }.toTypedArray()))
+    } catch (e: SQLiteConstraintException) {
+        throw IllegalArgumentException("There shouldn't be duplicate names in one game")
+    } catch (e: SQLiteAbortException) {
+        throw IllegalArgumentException("One of given players already exist in database")
+    }
 
     override suspend fun delete(player: Player) {
         playerDao.delete(player.toPlayerEntity())
     }
 
-    override suspend fun update(player: Player) {
+    override suspend fun update(player: Player) = try {
         playerDao.update(player.toPlayerEntity())
+    } catch (e: SQLiteConstraintException) {
+        throw IllegalArgumentException("There shouldn't be duplicate names in one game")
     }
 
     override fun getAllPlayersInGameSpecified(gameID: Int): Flow<List<Player>> =
