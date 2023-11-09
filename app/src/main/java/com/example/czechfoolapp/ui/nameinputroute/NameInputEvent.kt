33aff0b6d11970df568a33bd8abb96d867780c5a
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2023-11-09 20:57:57 +0400

    added Back event in NameInputEvent, which will be used to store current inputted names

--- app/src/main/java/com/example/czechfoolapp/ui/nameinputroute/NameInputEvent.kt
+++ app/src/main/java/com/example/czechfoolapp/ui/nameinputroute/NameInputEvent.kt
@@ -3,4 +3,5 @@ package com.example.czechfoolapp.ui.nameinputroute
 sealed interface NameInputEvent {
     data class PlayerNameChanged(val id: Int, val value: String) : NameInputEvent
     data class Next(val navigateToNext: () -> Unit) : NameInputEvent
+    data class Back(val navigateUp: () -> Unit) : NameInputEvent
 }
