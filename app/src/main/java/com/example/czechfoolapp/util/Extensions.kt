0ea3325318c5675fa52dabfe0b4507aa47bd74a0
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2023-11-09 20:55:39 +0400

    added Map<Int, PlayerNameState>.getDuplicates function, that returns keys that have duplicate values

--- app/src/main/java/com/example/czechfoolapp/util/Extensions.kt
+++ app/src/main/java/com/example/czechfoolapp/util/Extensions.kt
@@ -12,6 +12,19 @@ fun Map<Int, PlayerNameState>.toPlayersList(gameID: Int) = this
         Player(
             gameId = gameID,
             playerId = id,
-            name = playerNameState.name
+            name = playerNameState.name.trim()
         )
     }
+
+fun Map<Int, PlayerNameState>.getDuplicates(): List<Int> {
+    val seenValues: MutableList<String> = mutableListOf()
+    val duplicateKeys: MutableList<Int> = mutableListOf()
+    this.forEach {
+        if (seenValues.contains(it.value.name)) {
+            duplicateKeys.add(it.key)
+        } else {
+            seenValues.add(it.value.name)
+        }
+    }
+    return duplicateKeys.toList()
+}
