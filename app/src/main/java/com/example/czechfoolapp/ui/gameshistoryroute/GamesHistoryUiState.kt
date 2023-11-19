d5f07911b19d19602e1001dc6fbcb633747a8638
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2023-11-19 22:14:11 +0400

    added GamesHistoryUiState, which will represent state of this screen

---
+++ app/src/main/java/com/example/czechfoolapp/ui/gameshistoryroute/GamesHistoryUiState.kt
@@ -0,0 +1,20 @@
+package com.example.czechfoolapp.ui.gameshistoryroute
+
+import java.time.LocalDateTime
+
+data class PlayerUiModel(
+    val name: String,
+    val score: Int
+)
+
+data class GameUiModel(
+    val gameId: Int,
+    val losingScore: Int,
+    val date: LocalDateTime,
+    val isFinished: Boolean,
+    val players: List<PlayerUiModel>
+)
+
+data class GamesHistoryUiState(
+    val games: List<GameUiModel>
+)
