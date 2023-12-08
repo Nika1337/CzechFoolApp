8e840fd0fbd8f758d6dd7cc01c3bc48aa0bb83c5
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2023-12-08 20:20:51 +0400

    added logo to top app bar and remove colors, so it's default color now

--- app/src/main/java/com/example/czechfoolapp/ui/CzechFoolTopAppBar.kt
+++ app/src/main/java/com/example/czechfoolapp/ui/CzechFoolTopAppBar.kt
@@ -1,37 +1,31 @@
 package com.example.czechfoolapp.ui
 
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.dimensionResource
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.unit.sp
 import com.example.czechfoolapp.R
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun CzechFoolTopAppBar(
+    modifier: Modifier = Modifier,
     canNavigateBack: Boolean = false,
     onNavigateUp: () -> Unit = {},
-    modifier: Modifier = Modifier,
-    branding: @Composable () -> Unit = { Branding() }
+    branding: @Composable () -> Unit = { Branding(modifier = modifier.height(dimensionResource(R.dimen.logo_height))) }
 ) {
     CenterAlignedTopAppBar(
         title = branding,
-        colors = TopAppBarDefaults.mediumTopAppBarColors(
-            containerColor = MaterialTheme.colorScheme.primary
-        ),
         modifier = modifier.fillMaxWidth(),
         navigationIcon = {
             if (canNavigateBack) {
@@ -49,18 +43,9 @@ fun CzechFoolTopAppBar(
 
 @Composable
 private fun Branding(modifier: Modifier = Modifier) {
-    Row(
-        modifier = modifier,
-        horizontalArrangement = Arrangement.Center,
-        verticalAlignment = Alignment.Bottom
-    ){
-        Text(
-            text = "Czech",
-            fontSize = 32.sp
-        )
-        Text(
-            text = "Fool",
-            fontSize = 16.sp
-        )
-    }
+    Image(
+        painter = painterResource(R.drawable.czech_fool_logo),
+        contentDescription = stringResource(R.string.czech_fool),
+        modifier = modifier
+    )
 }
