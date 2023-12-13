91bdb82559c9d035787015993f6c2c7b10d3d1a6
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2023-12-13 18:45:26 +0400

    added ne composables that are used in both GameDetailScreen and GamesHistoryScreen

---
+++ app/src/main/java/com/example/czechfoolapp/ui/gameshistoryroute/composables/LosingScore.kt
@@ -0,0 +1,22 @@
+package com.example.czechfoolapp.ui.gameshistoryroute.composables
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.style.TextOverflow
+
+@Composable
+fun LosingScore(
+    score: Int,
+    style: TextStyle,
+    modifier: Modifier = Modifier
+) {
+    Text(
+        text = "Losing Score: $score",
+        style = style,
+        maxLines = 1,
+        overflow = TextOverflow.Ellipsis,
+        modifier = modifier
+    )
+}
