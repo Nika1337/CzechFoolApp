a923c67abfae17093500788051eaaf6e68262847
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2024-02-21 02:25:59 +0400

    deleted AppContainer.kt and removed unnecessary things from CzechFoolApplication

--- app/src/main/java/com/example/czechfoolapp/CzechFoolApplication.kt
+++ app/src/main/java/com/example/czechfoolapp/CzechFoolApplication.kt
@@ -1,14 +1,7 @@
 package com.example.czechfoolapp
 
 import android.app.Application
-import com.example.czechfoolapp.di.AppContainer
-import com.example.czechfoolapp.di.DefaultAppContainer
+import dagger.hilt.android.HiltAndroidApp
 
-class CzechFoolApplication : Application() {
-    lateinit var container: AppContainer
-
-    override fun onCreate() {
-        super.onCreate()
-        container = DefaultAppContainer(this)
-    }
-}
+@HiltAndroidApp
+class CzechFoolApplication : Application()
