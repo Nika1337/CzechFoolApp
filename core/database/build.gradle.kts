532edfb9dd1240791a52f26b458a577992551025
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2024-03-06 18:43:42 +0400

    applied necessary plugins to :core:database modules build.gradle.kts

--- core/database/build.gradle.kts
+++ core/database/build.gradle.kts
@@ -1,10 +1,11 @@
 plugins {
-    id("com.android.application")
+    alias(libs.plugins.czechfoolapp.android.library)
+    alias(libs.plugins.czechfoolapp.android.hilt)
+    alias(libs.plugins.czechfoolapp.android.room)
 }
 
 android {
     namespace = "com.example.czechfoolapp.core.database"
-
 }
 
 dependencies {
