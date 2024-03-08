90a23c94c2047d927b4e60ba20eaa2b7e5b1cf13
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2024-03-08 15:44:10 +0400

    added data module

---
+++ core/data/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    alias(libs.plugins.czechfoolapp.android.library)
+    alias(libs.plugins.czechfoolapp.android.hilt)
+}
+
+android {
+    namespace = "com.example.czechfoolapp.core.data"
+}
+
+dependencies {
+}
