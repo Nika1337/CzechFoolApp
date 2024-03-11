f2294b9f71adffe69f6b587912cc432da52894c9
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2024-03-11 14:04:42 +0400

    created :feature:gameshistory

---
+++ feature/gameshistory/build.gradle.kts
@@ -0,0 +1,11 @@
+plugins {
+    alias(libs.plugins.czechfoolapp.android.feature)
+    alias(libs.plugins.czechfoolapp.android.library.compose)
+}
+
+android {
+    namespace = "com.example.czechfoolapp.feature.gameshistory"
+}
+
+dependencies {
+}
