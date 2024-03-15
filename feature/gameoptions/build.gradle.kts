8bf11c0d8c18520b286d3984624ee800208f2cd8
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2024-03-15 13:27:21 +0400

    added :feature:gameoptions module

---
+++ feature/gameoptions/build.gradle.kts
@@ -0,0 +1,13 @@
+plugins {
+    alias(libs.plugins.czechfoolapp.android.feature)
+    alias(libs.plugins.czechfoolapp.android.library.compose)
+}
+
+android {
+    namespace = "com.example.czechfoolapp.feature.gameoptions"
+
+}
+
+dependencies {
+    
+}
