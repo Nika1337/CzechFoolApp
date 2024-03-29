836cf424a41b33c018288f0a32ae213d98ccd3ad
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2024-03-29 17:02:10 +0400

    moved DefaultCurrentGameDataSourceTest to :core:datastore

--- core/datastore/build.gradle.kts
+++ core/datastore/build.gradle.kts
@@ -4,10 +4,18 @@ plugins {
 }
 
 android {
+    defaultConfig {
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+    }
     namespace = "com.example.czechfoolapp.core.datastore"
 }
 
 dependencies {
     api(libs.androidx.dataStore.core)
     api(project(":core:datastore-proto"))
+
+    androidTestImplementation(kotlin("test"))
+    implementation(libs.kotlinx.coroutines.test)
+    androidTestImplementation(libs.androidx.test.ext)
+    androidTestImplementation(libs.androidx.test.runner)
 }
--- core/datastore/src/androidTest/kotlin/com/example/czechfoolapp/core/datastore/DefaultCurrentGameDataSourceTest.kt
+++ core/datastore/src/androidTest/kotlin/com/example/czechfoolapp/core/datastore/DefaultCurrentGameDataSourceTest.kt
@@ -1,21 +1,19 @@
-package com.example.czechfoolapp
+package com.example.czechfoolapp.core.datastore
+
 
 import androidx.datastore.core.DataStoreFactory
 import androidx.test.ext.junit.runners.AndroidJUnit4
-import com.example.czechfoolapp.datastore.CurrentGameDataSerializer
-import com.example.czechfoolapp.datastore.CurrentGameDataSource
-import com.example.czechfoolapp.datastore.DefaultCurrentGameDataSource
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.first
 import kotlinx.coroutines.test.TestScope
 import kotlinx.coroutines.test.UnconfinedTestDispatcher
 import kotlinx.coroutines.test.runTest
-import org.junit.Assert.assertEquals
 import org.junit.Before
 import org.junit.Rule
 import org.junit.Test
 import org.junit.rules.TemporaryFolder
 import org.junit.runner.RunWith
+import kotlin.test.assertEquals
 
 private const val TEST_DATASTORE_NAME = "current_game_data_test.pb"
 @RunWith(AndroidJUnit4::class)
