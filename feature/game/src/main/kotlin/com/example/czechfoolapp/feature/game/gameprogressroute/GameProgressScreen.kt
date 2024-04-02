b33dfbbc5632b099c596494dbade3286249be998
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2024-04-02 21:43:10 +0400

    removed unused import

--- feature/game/src/main/kotlin/com/example/czechfoolapp/feature/game/gameprogressroute/GameProgressScreen.kt
+++ feature/game/src/main/kotlin/com/example/czechfoolapp/feature/game/gameprogressroute/GameProgressScreen.kt
@@ -5,7 +5,6 @@ import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Close
 import androidx.compose.material.icons.filled.Done
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
