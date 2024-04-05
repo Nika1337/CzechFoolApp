e2028c2e8697aff97e5953ebba57b3c250e401f1
Nika1337 <124390721+Nika1337@users.noreply.github.com>
2024-04-05 22:49:16 +0400

    cleaned up queries of GameDao

--- core/database/src/main/kotlin/com/example/czechfoolapp/core/database/dao/GameDao.kt
+++ core/database/src/main/kotlin/com/example/czechfoolapp/core/database/dao/GameDao.kt
@@ -19,21 +19,29 @@ interface GameDao {
     suspend fun delete(game: GameEntity)
     @Transaction
     @Query(
-        "SELECT * FROM game " +
-        "where game_id = :id "
+        """
+            SELECT *
+            FROM game
+            WHERE game.game_id = :id
+        """
     )
     fun getGame(id: Int): Flow<GameWithPlayers?>
     @Transaction
     @Query(
-        "SELECT * FROM game ORDER BY game_id DESC"
+        """
+            SELECT *
+            FROM game
+        """
     )
     fun getAllGames(): Flow<List<GameWithPlayers>>
-
-    @Query(
-        "SELECT MAX(game_id) FROM game"
-    )
-    suspend fun getMaxGameID(): Int
-
-    @Query("SELECT EXISTS(SELECT * FROM game WHERE game_id = :gameID)")
-    suspend fun doesGameExistByID(gameID: Int): Boolean
 }
+
+
+
+
+
+
+
+
+
+
